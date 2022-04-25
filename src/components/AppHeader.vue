4cc7ff240a7cc9c6e48af551c90f8ff8572878a7
edkiimrgnl <ehor.k@code-care.pro>
2022-04-25 19:44:31 +0300

    changes
    delete unnecessary files;
    add header;

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo-container">
+      <img class="header_logo" src="@/assets/selectel_logo.svg" alt="Selectel">
+    </div>
+    <div>
+      <p class="header_telephone">
+        {{ telephone }}
+      </p>
+      <p class="header_email">
+        {{ email }}
+      </p>
+    </div>
+  </a-layout-header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+      telephone: '8 800 555 06 75',
+      email: 'sales@selectel.ru'
+    }
+  }
+}
+</script>
